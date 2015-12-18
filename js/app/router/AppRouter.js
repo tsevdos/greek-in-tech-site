159460bac0bcf1030a0164016e379468ed6f27a9
Kostas Bariotis <konmpar@gmail.com>
2015-12-19 00:27:25 +0200

    add an initial loader, resolves #37

--- js/app/router/AppRouter.js
+++ js/app/router/AppRouter.js
@@ -2,18 +2,21 @@ define([
 	'collections/Entries',
 	'models/Entry',
 	'views/EntriesView',
+	'views/LoaderView',
 	'views/EntryView',
 	'text!data/entries.json',
 	'backbone'
-], function(Entries, EntryModel, EntriesView, EntryView, data) {
+], function(Entries, EntryModel, EntriesView, LoaderView, EntryView, data) {
 
 	return Backbone.Router.extend({
+		loader: new LoaderView(),
 		routes: {
 			'': 'showRandomEntry',
 			'entry/:id(/:title)': 'showEntry'
 		},
 
 		initialize: function() {
+			this.loader.render();
 			this.entriesData = JSON.parse(data);
 			this.entries = new Entries(this.entriesData);
 			var entriesView = new EntriesView({ collection: this.entries });
@@ -21,11 +24,13 @@ define([
 		},
 
 		showRandomEntry: function() {
+			this.loader.trigger('hide');
 			var randomEntryID = this.entries.getUnviewedEntryID();
 			this.navigate("entry/" + randomEntryID, { trigger: true });
 		},
 
 		showEntry: function(id) {
+			this.loader.trigger('hide');
 			var entry = this.entries.findWhere({ id : parseInt(id) });
 			this.navigate("entry/" + id + "/" +
 				entry.urlFriendlyTitle(), { trigger: true });
