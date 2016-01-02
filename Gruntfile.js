46d6989cb5585dc5df2c02b5e7e9dc90aa3aaeed
John Tsevdos <tsevdosjohn@gmail.com>
2016-01-02 13:30:20 +0200

    Blue theme applied

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,37 @@ module.exports = function(grunt){
 			options: {
 				run: true
 			}
+		},
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'none',
+					compass: true
+				},
+				files: {
+					'css/main.css': 'scss/main.scss'
+				}
+			}
+		},
+
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
 		}
+
 	});
 
 	//load grunt mocha task
 	grunt.loadNpmTasks('grunt-mocha');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	grunt.registerTask('default', ['mocha']);
+	grunt.registerTask('dev', ['watch']);
+	grunt.registerTask('styles', ['sass']);
+
 };
