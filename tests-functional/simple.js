33328111d96f19cc09d98f2fad17b5fea258d5d4
John Tsevdos <tsevdosjohn@gmail.com>
2016-09-17 13:26:53 +0300

    Lint functional tests

--- tests-functional/simple.js
+++ tests-functional/simple.js
@@ -1,60 +1,56 @@
-casper.test.begin('site navigation: entry changed on click', function suite(test){
+casper.test.begin('site navigation: entry changed on click', function suite(test) {
 	var title;
-	casper.start('http://127.0.0.1:8080', function() {
-		test.assert(this.status().currentHTTPStatus == 200, "page loads successfully");
+	casper.start('http://127.0.0.1:8080', function () {
+		test.assert(this.status().currentHTTPStatus === 200, 'page loads successfully');
 	});
 
 	casper.wait(3000);
 
-	casper.then(function() {
+	casper.then(function () {
 		title = this.fetchText('h1');
 		test.assertTruthy(title, 'title has value');
-		console.log(this.fetchText('h1'));
 		casper.click('body');
 	});
 
-	casper.then(function() {
+	casper.then(function () {
 		test.assertNotEquals(this.fetchText('h1'), title, 'title changes after click');
 		title = this.fetchText('h1');
-		console.log(this.fetchText('h1'));
 	});
 
-	casper.run(function() {
+	casper.run(function () {
 		test.done();
 	});
 });
 
 casper.test.begin('site navigation: random entry is shown when id was not found', function suite(test) {
-	casper.start('http://127.0.0.1:8080/index.html#entry/a_non_existing_id', function() {
-		test.assert(this.status().currentHTTPStatus === 200, "page loads successfully");
+	casper.start('http://127.0.0.1:8080/index.html#entry/a_non_existing_id', function () {
+		test.assert(this.status().currentHTTPStatus === 200, 'page loads successfully');
 	});
 
 	casper.wait(3000);
 
-	casper.then(function() {
-		console.log(this.fetchText('h1'));
+	casper.then(function () {
 		test.assertElementCount('article > *', 3, 'a non-empty random entry has been loaded');
 	});
 
-	casper.run(function() {
+	casper.run(function () {
 		test.done();
 	});
 });
 
 casper.test.begin('content: references are shown', function suite(test) {
-	casper.start('http://127.0.0.1:8080/index.html#entry/31', function() {
-		test.assert(this.status().currentHTTPStatus === 200, "page loads successfully");
+	casper.start('http://127.0.0.1:8080/index.html#entry/31', function () {
+		test.assert(this.status().currentHTTPStatus === 200, 'page loads successfully');
 	});
 
 	casper.wait(3000);
 
-	casper.then(function() {
-		console.log(this.fetchText('h1'));
+	casper.then(function () {
 		test.assertTruthy(this.exists('ul.references'), 'reference list exists');
 		test.assertElementCount('ul.references li', 2, 'there are two references for this entry');
 	});
 
-	casper.run(function() {
+	casper.run(function () {
 		test.done();
 	});
 });
